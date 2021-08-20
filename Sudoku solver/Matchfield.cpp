03f980da8c1549300303239e8e9f840739c761a9
Maximilian Purrucker <m.purrucker@tu-braunschweig.de>
2021-08-20 21:43:03 +0200

    Updated Matchfield.cpp for datastructure usage and modified print layout of a soduko field

--- Sudoku solver/Matchfield.cpp
+++ Sudoku solver/Matchfield.cpp
@@ -5,60 +5,88 @@ using std::string;
 using std::cout;
 using std::endl;
 
-/* Initialize data fields with default value 0 */
+/*  Initialize data fields with default value 0 */
 Matchfield::Matchfield(int size) :size(size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            field[i][j] = 0;
-        }
-    }
+
 }
 
-/* Print soduku field seperated by '|' and '-' */
+/*  Print soduku field seperated by '|' and '-' */
 void Matchfield::printField() {
     printTopLine();
     for (int i = 0; i < size; i ++) {
         printLine(i);
-        printTopLine();
+        if (i < size - 1){
+            printInterline();
+        } else {
+            printTopLine();
+        }
         //Insert separators for each 3x3 block
         if ((i + 1) % 3 == 0 && i < size -1) {
-            cout << endl;
-            printTopLine();
+            cout << "|";
+            for (int i = 0; i < size * 3; i++) {
+                cout << " ";
+            }
+            cout << "|" << endl;
+            printInterline();
         }
     }
 }
 
-/* Print top line of each line */
+/*  Print top line of top and bottom line */
 void Matchfield::printTopLine() {
-    for (int i = 0; i < size * 2 + 5; i++) {
+    for (int i = 0; i < size * 3 + 2; i++) {
         cout << "-";
     }
     cout << endl;
 }
 
-/* Print line specified by index and seperate each value by '|' */
+/*  Print lines between top and bottom with whitespace between every 3x3 block */
+void  Matchfield::printInterline() {
+    string line = "-------";
+    string space = "    ";
+    cout << line << space << line << space << line << endl;
+}
+
+/*  Print line specified by index and seperate each value by '|' */
 void Matchfield::printLine(int index) {
     string line = "|";
     for (int j = 0; j < size; j++) {
-        if (field[index][j] == 0) {
+        if (field[index][j].value == 0) {
             line += " |";
         } else {
-            line += std::to_string(field[index][j]) + "|";
+            line += std::to_string(field[index][j].value) + "|";
         }
         //Insert separators for each 3x3 block
         if ((j + 1) % 3 == 0 && j < size -1) {
-            line += " |";
+            line += "    |";
         }
     }
     cout << line << endl;
 }
 
-/* Set data field to given value specified by its x and y coordinates */
-void Matchfield::setField(int x, int y, int value) {
-    field[x][y] = value;
+/*  Set data field to given value specified by its x and y coordinates */
+void Matchfield::setCell(Coordinate coordinate) {
+    field[coordinate.row][coordinate.column].value = coordinate.value;
+    field[coordinate.row][coordinate.column].properties = coordinate.properties;
 }
 
-/* Get value of a data cell specified by its x and y coordinates */
-int Matchfield::getField(int x, int y) {
-    return field[x][y];
+/*  Get value of a data cell specified by its x and y coordinates 
+    Return value of the specified cell */
+int Matchfield::getCell(int x, int y) {
+    return field[x][y].value;
+}
+
+/*  Get dimonsion of this soduko matchfield 
+    Return size of the soduko field */
+int Matchfield::getSize() {
+    return size;
+}
+
+/*  Delete value of every cell */
+void Matchfield::reset() {
+    for (int row = 0; row < size; row++) {
+        for (int column = 0; column < size; column++) {
+            field[row][column].value = 0;
+        } 
+    }
 }
--- Sudoku solver/Matchfield.h
+++ Sudoku solver/Matchfield.h
@@ -1,16 +1,20 @@
 #ifndef MATCHFIELD_H
 #define MATCHFIELD_H
+    #include "Datastructures.h"
 /* Class to represent and print a soduku field */
     class Matchfield{
     private:
         int size;
-        int field[9][9];
+        Coordinate field[9][9];
         void printTopLine();
         void printLine(int index);
+        void printInterline();
     public:
         Matchfield(int size);
         void printField();
-        void setField(int x, int y, int value);
-        int getField(int x, int y);
+        void setCell(Coordinate);
+        int getCell(int x, int y);
+        int getSize();
+        void reset();
     };
 #endif
